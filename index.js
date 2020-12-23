00266f95df3605fa3ea84cb1a90eb66c64d591f3
paul <ajuzepaul@gmail.com>
2020-12-23 15:23:18 +0200

    Add proper links to their respective elements

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// hover effects on projects
 const projects = document.querySelectorAll('.project');
 const projectContent = document.querySelectorAll('.project-details');
 
@@ -8,5 +9,16 @@ projects.forEach((element, index) => {
 
   element.addEventListener("mouseenter", () => {
     projectContent[index].classList.add('reveal');
+  });
+});
+
+
+// click event for items in hamburger menu
+const mobileLinks = document.querySelectorAll('.mobile-links > *');
+
+mobileLinks.forEach(element => {
+  element.addEventListener('click', () => {
+    // trigger close menu button for mobile
+    document.getElementById("menu-checkbox").click();
   });
 });
