5f4a33b5d5418ae4f54e1e7e642addcd42f94210
paul <ajuzepaul@gmail.com>
2020-12-24 21:56:31 +0200

    Integrate formspree

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-// hover effects on projects
-const projects = document.querySelectorAll('.project');
-const projectContent = document.querySelectorAll('.project-details');
-
-projects.forEach((element, index) => {
-  element.addEventListener("mouseleave", () => {
-    projectContent[index].classList.remove('reveal');
-  });
-
-  element.addEventListener("mouseenter", () => {
-    projectContent[index].classList.add('reveal');
-  });
-});
-
-
 // click event for items in hamburger menu
 const mobileLinks = document.querySelectorAll('.mobile-links > *');
 
@@ -22,3 +7,10 @@ mobileLinks.forEach(element => {
     document.getElementById("menu-checkbox").click();
   });
 });
+
+// redirect message when contact form is submitted
+// const submitBtn = document.getElementById('submit-btn');
+
+// submitBtn.addEventListener('click', (e) => {
+//   console.log('submitted');
+// });
