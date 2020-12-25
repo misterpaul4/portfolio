50f661a1173a87bbc057de254142b4cad3619377
paul <ajuzepaul@gmail.com>
2020-12-25 12:21:40 +0200

    use js to handle form submission

--- index.js
+++ index.js
@@ -8,9 +8,71 @@ mobileLinks.forEach(element => {
   });
 });
 
-// redirect message when contact form is submitted
-// const submitBtn = document.getElementById('submit-btn');
 
-// submitBtn.addEventListener('click', (e) => {
-//   console.log('submitted');
-// });
+const form = document.getElementById('contact-form');
+const userName = document.getElementById('name');
+
+const formSuccess = {
+  messageHeader: 'Thank you Anna',
+  message: 'Your message has been sent. I will reach out to you as soon as possible.',
+  icon: 'checked.svg',
+  actionText: 'close',
+};
+
+const formFail = {
+  messageHeader: 'Whoops!',
+  message: "Something went wrong. Let's give this another try",
+  icon: 'retry.svg',
+  actionText: 'retry',
+}
+
+const buildStatus = (status) => {
+  const formParentContainer = document.querySelector('.contact');
+  const footer = document.getElementById('contact-me');
+  formParentContainer.classList.add('blur');
+
+  const container = document.createElement('div');
+  container.classList.add('form-status', 'container', 'centered');
+
+  container.innerHTML = `
+  <img src="./img/${status.icon}" alt="check icon" class="check-icon"/>
+  <div class="message">
+    <h1>${status.messageHeader}</h1>
+    <p>${status.message}</p>
+  </div>
+  <button class="close-btn">${status.actionText}</button>
+  `
+  footer.appendChild(container);
+}
+
+buildStatus(formSuccess);
+
+const submitForm = () => {
+  const XHR = new XMLHttpRequest();
+  const FD = new FormData(form);
+
+  // succesful
+  XHR.addEventListener('load', (e) => {
+    alert(e.target.responseText);
+  });
+
+  // unsuccesful
+  XHR.addEventListener('error', (e) => {
+    alert('Oops! Something went wrong.');
+  });
+
+  // set up request
+  XHR.open('POST', 'https://formspree.io/f/xdoppzgd');
+  XHR.setRequestHeader("Accept", "application/json");
+
+  // send data
+  XHR.send(FD);
+};
+
+// redirect message when contact form is submitted
+const submitBtn = document.getElementById('submit-btn');
+
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  submitForm();
+});
